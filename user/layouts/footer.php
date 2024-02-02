08eafc1d32cf9829387c6011a90855d48ca2a0e8
xmartina <martinsprince126@gmail.com>
2024-02-02 14:38:39 +0100

    p

--- user/layouts/footer.php
+++ user/layouts/footer.php
@@ -38,7 +38,7 @@
 <!-- END MAIN CONTAINER -->
 
 <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
-<script src="../assets/js/libs/jquery-3.1.1.min.js"></script>
+<script src="/assets/js/libs/jquery-3.1.1.min.js"></script>
 <script src="../bootstrap/js/popper.min.js"></script>
 <script src="../bootstrap/js/bootstrap.min.js"></script>
 <script src="../plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
